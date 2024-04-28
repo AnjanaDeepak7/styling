31024068212605629111e576ce2d5441432ce673
AnjanaDeepak7 <anjanadeepak60@gmail.com>
2024-04-28 11:52:54 +0530

    initial changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import fnf from "./fnf.webp";
+import "./App.css";
 
 function App() {
+  const mainStyle = {
+    backgroundColor: "gray",
+    color: "black",
+    padding: "0.5rem",
+    fontWeight: "bold",
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <h1
+          style={{
+            backgroundColor: "whitesmoke",
+            color: "black",
+            padding: "0.5rem 1rem",
+          }}
         >
-          Learn React
-        </a>
+          Fast and Furious
+        </h1>
+        <img src={fnf} className="App-logo" alt="logo" />
       </header>
+      <main style={mainStyle}>
+        <ul>
+          <li
+            style={{
+              listStyleType: "none",
+              fontSize: "2rem",
+            }}
+          >
+            Brian O Conner
+          </li>
+          <li
+            style={{
+              listStyleType: "none",
+              fontSize: "2rem",
+            }}
+          >
+            Vin Diesel
+          </li>
+        </ul>
+      </main>
     </div>
   );
 }
